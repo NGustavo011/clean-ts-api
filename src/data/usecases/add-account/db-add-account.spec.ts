4e5a0d01fe5a5b3acb6c009ded87da042895b41e
NGustavo011 <gsn181200@gmail.com>
2023-06-30 12:53:42 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -13,7 +13,7 @@ const makeAddAccountRepository = (): AddAccountRepository => {
       return await new Promise((resolve) => {
         resolve({
           id: 'valid_id',
-          email: 'valid_email',
+          email: 'valid_email@mail.com',
           name: 'valid_name',
           password: 'hashed_password'
         })
@@ -87,5 +87,20 @@ describe('DbAddAccount Usecase', () => {
     }
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
+  })
+  test('Deve retornar uma conta em caso de sucesso', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'hashed_password'
+    })
   })
 })
