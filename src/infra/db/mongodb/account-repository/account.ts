63bfd7fb616403d23a8c19fd9f1bbbb30bb6484f
NGustavo011 <gsn181200@gmail.com>
2023-07-04 14:40:29 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success and returns null when loadByEmail fails

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,10 @@
 import { type AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { type LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { type AccountModel } from '../../../../domain/models/account'
 import { type AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
@@ -12,6 +13,18 @@ export class AccountMongoRepository implements AddAccountRepository {
       name: accountData.name,
       email: accountData.email,
       password: accountData.password
+    }
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel | null> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+    if (!account) { return null }
+    return {
+      id: account._id as string,
+      name: account.name as string,
+      email: account.email as string,
+      password: account.password as string
     }
   }
 }
