c0c480306d47979ee623af1b3f772b99caa31c9e
NGustavo011 <gsn181200@gmail.com>
2023-07-05 15:16:58 -0300

    chore: create up script to use docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,7 @@
+import dotenv from 'dotenv'
+dotenv.config()
 export default {
-  port: process.env.PORT ?? 3333,
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://root:pass@localhost:27017',
+  port: process.env.PORT ?? 5000,
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://root:pass@mongo:27017',
   jwtSecret: process.env.JWT_SECRET ?? 'tj670==5H'
 }
