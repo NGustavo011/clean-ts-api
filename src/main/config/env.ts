569646645421de985550001a98d892d2bce4b1b5
NGustavo011 <gsn181200@gmail.com>
2023-07-05 11:03:07 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   port: process.env.PORT ?? 3333,
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://root:pass@localhost:27017'
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://root:pass@localhost:27017',
+  jwtSecret: process.env.JWT_SECRET ?? 'tj670==5H'
 }
