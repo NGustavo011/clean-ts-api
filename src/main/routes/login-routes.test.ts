7b6add2a2aea94b485eca9b23f7342762dff62a8
NGustavo011 <gsn181200@gmail.com>
2023-07-05 11:07:32 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
 import request from 'supertest'
 import { app } from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { type Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -10,7 +14,7 @@ describe('Login Routes', () => {
     await MongoHelper.disconnect()
   })
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
   describe('POST /signup', () => {
@@ -20,6 +24,20 @@ describe('Login Routes', () => {
         email: 'gustavo.nogueira@gmail.com',
         password: '123',
         passwordConfirmation: '123'
+      }).expect(200)
+    })
+  })
+  describe('POST /login', () => {
+    test('Deve retornar status code 200 em caso de sucesso no Login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Gustavo',
+        email: 'gustavo.nogueira@gmail.com',
+        password
+      })
+      await request(app).post('/api/login').send({
+        email: 'gustavo.nogueira@gmail.com',
+        password: '123'
       }).expect(200)
     })
   })
