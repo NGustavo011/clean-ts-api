b2f881ea86a480190f7d8ba5a90f38e1240a77a6
NGustavo011 <gsn181200@gmail.com>
2023-07-06 15:34:59 -0300

    test: ensure JwtAdapter returns a decrypted value on verify success

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -41,5 +41,10 @@ describe('Jwt Adapter', () => {
       await sut.decrypt('any_token')
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
+    test('Deve retornar um valor em caso de sucesso do método verify', async () => {
+      const sut = makeSut()
+      const value = await sut.decrypt('any_token')
+      expect(value).toBe('any_value')
+    })
   })
 })
