381410bc6d07d4f6f8496ec06406cf2b0259c5b0
NGustavo011 <gsn181200@gmail.com>
2023-07-06 16:41:59 -0300

    test: ensure add survey route returns 204 if valid accessToken is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,12 @@
 import request from 'supertest'
 import { app } from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
-import { type Collection } from 'mongodb'
+import { ObjectId, type Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -15,6 +18,8 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
   describe('POST /surveys', () => {
     test('Deve retornar status code 403 em caso de realizar a requisição sem passar o accessToken', async () => {
@@ -29,6 +34,34 @@ describe('Survey Routes', () => {
           }
         ]
       }).expect(403)
+    })
+    test('Deve retornar status code 204 em caso de sucesso', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Gustavo',
+        email: 'gu.nogueira@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const id = res.insertedId.toString()
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: new ObjectId(id)
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app).post('/api/surveys').set('x-access-token', accessToken).send({
+        question: 'Question',
+        answers: [
+          {
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          }, {
+            answer: 'Answer 2'
+          }
+        ]
+      }).expect(204)
     })
   })
 })
