db13ab5262c996be40816d5bd392ec7d564257a2
NGustavo011 <gsn181200@gmail.com>
2023-07-06 11:57:38 -0300

    feat: ensure AddSurveyController returns 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,4 +1,4 @@
-import { badRequest, serverError } from '../../../helpers/http/http-helper'
+import { badRequest, noContent, serverError } from '../../../helpers/http/http-helper'
 import { type Validation, type Controller, type HttpRequest, type HttpResponse, type AddSurvey } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
@@ -14,10 +14,7 @@ export class AddSurveyController implements Controller {
         question,
         answers
       })
-      return {
-        body: {},
-        statusCode: 0
-      }
+      return noContent()
     } catch (error) {
       return serverError(error)
     }
