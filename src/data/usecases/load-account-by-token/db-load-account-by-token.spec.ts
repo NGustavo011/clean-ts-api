e7d56d706bab1b00493ab866d7fa127d2f50f1a7
NGustavo011 <gsn181200@gmail.com>
2023-07-06 15:00:28 -0300

    test: ensure DbLoadAccountByToken returns null if Decrypter returns null

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -8,7 +8,7 @@ interface SutTypes {
 
 const makeDecrypterStub = (): Decrypter => {
   class DecrypterStub implements Decrypter {
-    async decrypt (value: string): Promise<string> {
+    async decrypt (value: string): Promise<string | null> {
       return await new Promise(resolve => { resolve('any_value') })
     }
   }
@@ -28,7 +28,13 @@ describe('DbLoadAccountByToken Usecase', () => {
   test('Deve chamar o Decrypter com os valores corretos', async () => {
     const { sut, decrypterStub } = makeSut()
     const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
-    await sut.load('any_token')
+    await sut.load('any_token', 'any_role')
     expect(decryptSpy).toHaveBeenCalledWith('any_token')
+  })
+  test('Deve retornar null se o Decrypter retornar null', async () => {
+    const { sut, decrypterStub } = makeSut()
+    jest.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(new Promise(resolve => { resolve(null) }))
+    const account = await sut.load('any_token', 'any_role')
+    expect(account).toBeNull()
   })
 })
