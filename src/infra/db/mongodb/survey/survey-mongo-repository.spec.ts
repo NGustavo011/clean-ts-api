7012d52dcce56b19872273de0c4f8ec893ca7d14
NGustavo011 <gsn181200@gmail.com>
2023-07-08 08:37:20 -0300

    refactor: organize SurveyMongoRepository tests in describes

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -4,6 +4,11 @@ import { SurveyMongoRepository } from './survey-mongo-repository'
 import { type AddSurveyModel } from '../../../../domain/usecases/add-survey'
 
 let surveyCollection: Collection
+
+const makeSut = (): SurveyMongoRepository => {
+  return new SurveyMongoRepository()
+}
+
 describe('Survey Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
@@ -16,26 +21,44 @@ describe('Survey Mongo Repository', () => {
     await surveyCollection.deleteMany({})
   })
 
-  const makeSut = (): SurveyMongoRepository => {
-    return new SurveyMongoRepository()
-  }
-
-  test('Deve realizar com sucesso o método de add', async () => {
-    const sut = makeSut()
-    const fakeSurveyData: AddSurveyModel = {
-      question: 'any_question',
-      answers: [
-        {
-          image: 'any_image',
-          answer: 'any_answer'
-        }, {
-          answer: 'other_answer'
-        }
-      ],
-      date: new Date()
-    }
-    await sut.add(fakeSurveyData)
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
+  describe('add()', () => {
+    test('Deve realizar com sucesso o método de add', async () => {
+      const sut = makeSut()
+      const fakeSurveyData: AddSurveyModel = {
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'any_answer'
+          }, {
+            answer: 'other_answer'
+          }
+        ],
+        date: new Date()
+      }
+      await sut.add(fakeSurveyData)
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
+    })
+  })
+  describe('add()', () => {
+    test('Deve realizar com sucesso o método de add', async () => {
+      const sut = makeSut()
+      const fakeSurveyData: AddSurveyModel = {
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'any_answer'
+          }, {
+            answer: 'other_answer'
+          }
+        ],
+        date: new Date()
+      }
+      await sut.add(fakeSurveyData)
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
+    })
   })
 })
