65f9d99c47f03ed69d13649c0cfbb4312bb8b25b
NGustavo011 <gsn181200@gmail.com>
2023-07-08 13:52:03 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
@@ -1,3 +1,5 @@
 export * from '../../../../domain/usecases/survey/load-survey-by-id'
+export * from '../../../../domain/usecases/survey-result/save-survey-result'
 export * from '../../../protocols'
 export * from '../../../../domain/models/survey'
+export * from '../../../../domain/models/survey-result'
--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,19 +1,26 @@
 import { InvalidParamError } from '../../../errors'
 import { forbidden, serverError } from '../../../helpers/http/http-helper'
-import { type LoadSurveyById, type Controller, type HttpRequest, type HttpResponse } from './save-survey-result-controller-protocols'
+import { type LoadSurveyById, type Controller, type HttpRequest, type HttpResponse, type SaveSurveyResult } from './save-survey-result-controller-protocols'
 
 export class SaveSurveyResultController implements Controller {
-  constructor (private readonly loadSurveyById: LoadSurveyById) {
+  constructor (private readonly loadSurveyById: LoadSurveyById, private readonly saveSurveyResult: SaveSurveyResult) {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params
       const { answer } = httpRequest.body
+      const { accountId } = httpRequest
       const survey = await this.loadSurveyById.loadById(surveyId)
       if (!survey) { return forbidden(new InvalidParamError('surveyId')) }
       const answers = survey.answers.map(a => a.answer)
       if (!answers.includes(answer)) { return forbidden(new InvalidParamError('answer')) }
+      await this.saveSurveyResult.save({
+        accountId: accountId as string,
+        surveyId,
+        answer,
+        date: new Date()
+      })
       return {
         body: {},
         statusCode: 0
