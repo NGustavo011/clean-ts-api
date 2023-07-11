2400894077379a0b56f749bc71a4fa044b89a0b0
NGustavo011 <gsn181200@gmail.com>
2023-07-11 14:46:55 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -1,4 +1,43 @@
 export const surveyResultPath = {
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Enquete'],
+    summary: 'Rota para consultar o resultado de uma enquete',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      401: {
+        $ref: '#components/unauthorized'
+      },
+      403: {
+        $ref: '#components/forbidden'
+      },
+      404: {
+        $ref: '#components/notFound'
+      },
+      500: {
+        $ref: '#components/serverError'
+      }
+    }
+  },
   put: {
     security: [{
       apiKeyAuth: []
@@ -8,7 +47,7 @@ export const surveyResultPath = {
     parameters: [{
       in: 'path',
       name: 'surveyId',
-      require: true,
+      required: true,
       schema: {
         type: 'string'
       }
