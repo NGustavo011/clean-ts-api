96463e9f7573cc4100da3059af73a4b4e2909693
NGustavo011 <gsn181200@gmail.com>
2023-07-11 11:39:13 -0300

    test: ensure DbLoadSurveyResult returns a surveyResult on success

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -1,7 +1,8 @@
-import { throwError } from '../../../../domain/test'
+import { mockSurveyResultModel, throwError } from '../../../../domain/test'
 import { mockLoadSurveyResultRepository } from '../../../test'
 import { DbLoadSurveyResult } from './db-load-survey-result'
 import { type LoadSurveyResultRepository } from './db-load-survey-result-protocols'
+import MockDate from 'mockdate'
 
 interface SutTypes {
   sut: DbLoadSurveyResult
@@ -18,6 +19,12 @@ const makeSut = (): SutTypes => {
 }
 
 describe('DbLoadSurveyResult Usecase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+  afterAll(() => {
+    MockDate.reset()
+  })
   test('Deve chamar o LoadSurveyResultRepository', async () => {
     const { sut, loadSurveyResultRepositoryStub } = makeSut()
     const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
@@ -30,10 +37,9 @@ describe('DbLoadSurveyResult Usecase', () => {
     const promise = sut.load('any_survey_id')
     await expect(promise).rejects.toThrow()
   })
-  test('Deve repassar o error caso o LoadSurveyResultRepository lance um erro', async () => {
-    const { sut, loadSurveyResultRepositoryStub } = makeSut()
-    jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId').mockImplementationOnce(throwError)
-    const promise = sut.load('any_survey_id')
-    await expect(promise).rejects.toThrow()
+  test('Deve retornar um surveyResultModel on success', async () => {
+    const { sut } = makeSut()
+    const surveyResult = await sut.load('any_survey_id')
+    expect(surveyResult).toEqual(mockSurveyResultModel())
   })
 })
